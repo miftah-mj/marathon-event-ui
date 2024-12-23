9d0041dfd1dad516c8fd8c31316208fd32119c20
miftah1299 <jannat27120@gmail.com>
2024-12-24 02:15:38 +0600

    enhance Dashboard component with navigation links and layout structure

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,8 +1,50 @@
+import { NavLink, Outlet } from "react-router-dom";
 
 const Dashboard = () => {
     return (
-        <div>
-            
+        <div className="dashboard">
+            <header className="header p-4">
+                <h1 className="text-3xl font-bold">Dashboard</h1>
+            </header>
+
+            <div className="flex gap-10 lg:gap-20 items-center">
+                <nav className="nav p-4 flex flex-col gap-4 items-start">
+                    <NavLink
+                        to="/dashboard/add-marathon"
+                        className={({ isActive }) =>
+                            `tab ${
+                                isActive ? "text-accent" : "hover:text-accent"
+                            }`
+                        }
+                    >
+                        Add Marathon
+                    </NavLink>
+                    <NavLink
+                        to="/dashboard/marathon-list"
+                        className={({ isActive }) =>
+                            `tab ${
+                                isActive ? "text-accent" : "hover:text-accent"
+                            }`
+                        }
+                    >
+                        My Marathon List
+                    </NavLink>
+                    <NavLink
+                        to="/dashboard/apply-list"
+                        className={({ isActive }) =>
+                            `tab ${
+                                isActive ? "text-accent" : "hover:text-accent"
+                            }`
+                        }
+                    >
+                        My Apply List
+                    </NavLink>
+                </nav>
+
+                <div>
+                    <Outlet />
+                </div>
+            </div>
         </div>
     );
 };
